57273683935ed2f54b500c8000eb2b569d0d3c89
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-01-19 14:30:49 -0500

    Add state validation test

--- model/src/test/scala/hmda/model/filing/ts/validation/TsModelValidatorSpec.scala
+++ model/src/test/scala/hmda/model/filing/ts/validation/TsModelValidatorSpec.scala
@@ -1,10 +1,12 @@
 package hmda.model.filing.ts.validation
 
-import cats.data.Validated.Valid
+import cats.implicits._
 import org.scalatest.{MustMatchers, PropSpec}
 import org.scalatest.prop.PropertyChecks
 import hmda.model.filing.ts.TsGenerators._
 import hmda.model.filing.ts.validation.TsModelValidator._
+import hmda.model.filing.ts.validation.TsValidationErrorModel.TsStateCodeNotRecognized
+import org.scalacheck.Gen
 
 class TsModelValidatorSpec
     extends PropSpec
@@ -13,7 +15,15 @@ class TsModelValidatorSpec
 
   property("An address must be valid") {
     forAll(addressGen) { address =>
-      validateAddress(address) mustBe Valid(address)
+      validateAddress(address) mustBe address.validNel
+    }
+  }
+
+  property("An address with malformed state abbreviation must be invalid") {
+    forAll(addressGen) { address =>
+      val badState = Gen.alphaStr.sample.getOrElse("XX")
+      val badAddress = address.copy(state = badState)
+      validateAddress(badAddress) mustBe TsStateCodeNotRecognized.invalidNel
     }
   }
 }
