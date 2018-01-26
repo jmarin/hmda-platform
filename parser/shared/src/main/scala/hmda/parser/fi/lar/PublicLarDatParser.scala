a21b6f0f2f34d59b8641084e5f6537f254e60f2a
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-01-26 16:42:04 -0500

    Add files

---
+++ parser/shared/src/main/scala/hmda/parser/fi/lar/PublicLarDatParser.scala
@@ -0,0 +1,16 @@
+package hmda.parser.fi.lar
+
+import hmda.model.fi.lar.LoanApplicationRegister
+
+// Parses public LAR data
+object PublicLarDatParser {
+  def apply(s: String): LoanApplicationRegister = {
+
+
+
+
+
+    LoanApplicationRegister()
+
+  }
+}
---
+++ parser/shared/src/test/scala/hmda/parser/fi/lar/PublicLarDatParserSpec.scala
@@ -0,0 +1,13 @@
+package hmda.parser.fi.lar
+
+import org.scalatest.prop.PropertyChecks
+import org.scalatest.{MustMatchers, PropSpec }
+
+class PublicLarDatParserSpec extends PropSpec with PropertyChecks with MustMatchers {
+
+  property("Public LAR DAT file must parse") {
+
+  }
+
+
+}
