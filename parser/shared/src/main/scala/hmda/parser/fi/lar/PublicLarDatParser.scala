38033443af8748968748cff8046bb4baf5f4e8e9
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-01-28 12:04:30 -0500

    Add loan parsing

--- parser/shared/src/main/scala/hmda/parser/fi/lar/PublicLarDatParser.scala
+++ parser/shared/src/main/scala/hmda/parser/fi/lar/PublicLarDatParser.scala
@@ -1,16 +1,43 @@
 package hmda.parser.fi.lar
 
-import hmda.model.fi.lar.LoanApplicationRegister
+import hmda.model.fi.lar.{ Loan, LoanApplicationRegister }
+
+import scala.util.{ Failure, Success, Try }
 
 // Parses public LAR data
 object PublicLarDatParser {
   def apply(s: String): LoanApplicationRegister = {
 
+    val respId = s.substring(4, 14)
+    val code = toSafeInt(s.substring(14, 15))
+    val loanType = toSafeInt(s.substring(15, 16))
+    val loanPurpose = toSafeInt(s.substring(16, 17))
+    val occupancy = toSafeInt(s.substring(17, 18))
+    val amount = toSafeInt(s.substring(18, 23))
+    val actionTakenType = toSafeInt(s.substring(23, 24))
 
+    val loan = Loan(
+      "",
+      "",
+      loanType,
+      0,
+      loanPurpose,
+      occupancy,
+      amount
+    )
 
+    LoanApplicationRegister(
+      respondentId = respId,
+      agencyCode = code,
+      loan = loan
+    )
 
+  }
 
-    LoanApplicationRegister()
-
+  private def toSafeInt(s: String): Int = {
+    Try(s.toInt) match {
+      case Success(i) => i
+      case Failure(_) => throw new Exception(s"Cannot convert $s to Integer")
+    }
   }
 }
--- parser/shared/src/test/scala/hmda/parser/fi/lar/PublicLarDatParserSpec.scala
+++ parser/shared/src/test/scala/hmda/parser/fi/lar/PublicLarDatParserSpec.scala
@@ -1,13 +1,35 @@
 package hmda.parser.fi.lar
 
+import hmda.model.fi.lar.Loan
 import org.scalatest.prop.PropertyChecks
-import org.scalatest.{MustMatchers, PropSpec }
+import org.scalatest.{ MustMatchers, PropSpec }
 
 class PublicLarDatParserSpec extends PropSpec with PropertyChecks with MustMatchers {
 
+  val larStr = "20130000000324113200755310900420770014.0112005703  613225    5    NA   210000194\n" +
+    "20130000000324111100370120764340297390.004500300   613457    8    NA   210000110"
+
+  val lars = larStr.split("\n")
+
   property("Public LAR DAT file must parse") {
+    val parsedLars = lars
+      .map(str => PublicLarDatParser(str))
+
+    parsedLars
+      .map { lar =>
+        lar.respondentId mustBe "0000000324"
+        lar.agencyCode mustBe 1
+      }
+
+    val lar1 = parsedLars.head
+    val lar2 = parsedLars.tail.head
+
+    lar1
+      .loan mustBe Loan("", "", 1, 0, 3, 2, 755)
+
+    lar2
+      .loan mustBe Loan("", "", 1, 0, 1, 1, 370)
 
   }
-
 
 }
