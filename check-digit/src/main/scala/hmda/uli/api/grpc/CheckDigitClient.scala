936aa54ebe8c80e5267e5d8f58fcadb8b4d9d554
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-06-24 23:46:55 -0400

    catch exceptions and close channel for gRPC client

--- check-digit/src/main/scala/hmda/uli/api/grpc/CheckDigitClient.scala
+++ check-digit/src/main/scala/hmda/uli/api/grpc/CheckDigitClient.scala
@@ -16,11 +16,18 @@ object CheckDigitClient {
       .forAddress("localhost", port)
       .usePlaintext()
       .build()
-    val request = CheckDigitRequest("10Bx939c5543TqA1144M999143X")
-    val stub = CheckDigitServiceGrpc.stub(channel)
-    val checkDigitF = stub.checkDigit(request)
-    val response = Await.result(checkDigitF, 2.seconds)
-    println(response)
+    try {
+      val request = CheckDigitRequest("1A1144M999143X")
+      val stub = CheckDigitServiceGrpc.stub(channel)
+      val checkDigitF = stub.checkDigit(request)
+      val response = Await.result(checkDigitF, 2.seconds)
+      println(response)
+    } catch {
+      case e: Exception =>
+        println(e.getLocalizedMessage)
+    } finally {
+      channel.shutdown()
+    }
   }
 
 }
