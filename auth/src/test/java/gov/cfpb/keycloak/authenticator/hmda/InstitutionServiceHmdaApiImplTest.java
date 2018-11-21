5384a596f651238d41dc69361eec7085d64e0ec2
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-11-20 19:04:10 -0500

    Configure testing

---
+++ auth/src/test/java/gov/cfpb/keycloak/authenticator/hmda/InstitutionServiceHmdaApiImplTest.java
@@ -0,0 +1,17 @@
+package gov.cfpb.keycloak.authenticator.hmda;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class InstitutionServiceHmdaApiImplTest {
+
+    @Test
+    @DisplayName("\uD83D\uDE31")
+    void testGetInstitutions(){
+        System.out.println("Testing Institutions Service Client");
+        assertEquals(2, 2);
+    }
+}
