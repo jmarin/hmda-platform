ee813499ba31ea6db572595a44b8998d3c93aa6e
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-11-20 21:49:11 -0500

    Configure JUnit

---
+++ auth/src/test/java/gov/cfpb/keycloak/authenticator/hmda/InstitutionServiceTest.java
@@ -0,0 +1,14 @@
+package gov.cfpb.keycloak.authenticator.hmda;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class InstitutionServiceTest {
+
+    @Test
+    void testGetInstitutions() {
+        System.out.println("Test Get Institutions");
+        assertEquals(2, 2);
+    }
+}
