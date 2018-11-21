fa94d2ea37088dbab71f9ec3cc701285c4cb32e2
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-11-20 19:16:40 -0500

    Update config to Java 8

--- auth/src/test/java/gov/cfpb/keycloak/authenticator/hmda/InstitutionServiceHmdaApiImplTest.java
+++ auth/src/test/java/gov/cfpb/keycloak/authenticator/hmda/InstitutionServiceHmdaApiImplTest.java
@@ -4,6 +4,8 @@ package gov.cfpb.keycloak.authenticator.hmda;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InstitutionServiceHmdaApiImplTest {
@@ -12,6 +14,19 @@ class InstitutionServiceHmdaApiImplTest {
     @DisplayName("\uD83D\uDE31")
     void testGetInstitutions(){
         System.out.println("Testing Institutions Service Client");
-        assertEquals(2, 2);
+        //assertEquals(2, 2);
+
+        String url = "https://ffiec-api.cfpb.gov/public";
+
+        InstitutionService institutionService = new InstitutionServiceHmdaApiImpl(url, false);
+
+        try {
+            List<Institution> institutions = institutionService.findInstitutionsByDomain("bankofamerica.com");
+            institutions.forEach(e -> e.getName());
+        } catch (InstitutionServiceException e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+
+
     }
 }
